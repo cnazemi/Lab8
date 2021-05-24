f6961b299ac64f13f4aaa2db7d2de8dcb4ff4ae1
cnazemi <cameronsnazemi@gmail.com>
2021-05-23 23:47:48 -0700

    part 2 complete, updated readme, added screenshot of tests

--- README.md
+++ README.md
@@ -1,21 +1,25 @@
 # Lab8_Starter
 
+## Author(s):
+- Cameron Nazemi
+
 ## Check your understanding q's (FILL OUT)
-1. In your own words: Where would you fit your automated tests in your Bujo project development pipeline? (just write the letter)
+1. In your own words: Where would you fit your automated tests in your Bujo project development pipeline? (just write the letter)<br />
 1
 
 
-2. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.
+2. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.<br />
 No because a message feature would be an application/feature and would testable with end-to-end testing because it "tries to replicate a user’s workflow from start to finish". Sending a message to another user would be more interactive.
 
-3. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters
+3. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters <br />
 Yes I would write a unit test, since its a small scale feature without many moving parts and can be executed quickly, also changes in the app in other sections wouldn't affect this testing.
 
-4. What do you expect to happen if we run our puppeteer tests with the field “headless” set to true?
+4. What do you expect to happen if we run our puppeteer tests with the field “headless” set to true?<br />
 I would expect the puppeteer tests to be run without a browser UI
 
-5. What would your beforeAll callback look like if you wanted to start from the settings page before every test case?
+5. What would your beforeAll callback look like if you wanted to start from the settings page before every test case? <br />
 beforeAll(async () => {
-    await settingsPage(true);
+    page.click('header > img')
+    await page.waitForNavigation();
     await page.waitForTimeout(500);
   });
--- __tests__/lab8.test.js
+++ __tests__/lab8.test.js
@@ -29,12 +29,26 @@ describe('Basic user flow for SPA ', () => {
 
   it('Test3: Clicking first <journal-entry>, new URL should contain /#entry1', async () => {
     // implement test3: Clicking on the first journal entry should update the URL to contain “/#entry1”
+    
 
+    const entries = await page.$$("journal-entry");
+    // Get the first entry
+    const entry1 = entries[0];
+    
+    await entry1.click();
+    // awaiting navigation
+    await page.waitForNavigation();
+    
+    // Check if page url matches first entry
+    expect(page.url()).toBe("http://127.0.0.1:5500/#entry1");
   });
 
   it('Test4: On first Entry page - checking page header title', async () => {
     // implement test4: Clicking on the first journal entry should update the header text to “Entry 1” 
+   
+    const entry1Title = await page.$eval("header > h1", (headers) => headers.innerHTML);
 
+    expect(entry1Title).toBe("Entry 1");
   });
 
   it('Test5: On first Entry page - checking <entry-page> contents', async () => {
@@ -51,57 +65,169 @@ describe('Basic user flow for SPA ', () => {
         }
       */
 
+ 
+    
+    const entry = await page.$eval("entry-page", (entry1) => entry1.entry);
+    
+    
+    expect(entry).toEqual({ 
+                  title: 'You like jazz?',
+                  date: '4/25/2021',
+                  content: "According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground. The bee, of course, flies anyway because bees don't care what humans think is impossible.",
+                  image: {
+                    src: 'https://i1.wp.com/www.thepopcornmuncher.com/wp-content/uploads/2016/11/bee-movie.jpg?resize=800%2C455',
+                    alt: 'bee with sunglasses'
+                  } 
+                });
+
   }, 10000);
 
   it('Test6: On first Entry page - checking <body> element classes', async () => {
     // implement test6: Clicking on the first journal entry should update the class attribute of <body> to ‘single-entry’
-
+    const bodyAttr = await page.$eval("body", (body) => body.className);
+    expect(bodyAttr).toBe("single-entry");
   });
 
   it('Test7: Clicking the settings icon, new URL should contain #settings', async () => {
     // implement test7: Clicking on the settings icon should update the URL to contain “/#settings”
+    page.click('header > img')
+    await page.waitForNavigation();
+    
+    
+    // Check if page url matches first entry
+    expect(page.url()).toBe("http://127.0.0.1:5500/#settings");
 
   });
 
   it('Test8: On Settings page - checking page header title', async () => {
     // implement test8: Clicking on the settings icon should update the header to be “Settings”
+    const settingsTitle = await page.$eval("header > h1", (headers) => headers.innerHTML);
 
+    expect(settingsTitle).toBe("Settings");
   });
 
   it('Test9: On Settings page - checking <body> element classes', async () => {
     // implement test9: Clicking on the settings icon should update the class attribute of <body> to ‘settings’
-
+    const settingsBodyAttr = await page.$eval("body", (body) => body.className);
+    expect(settingsBodyAttr).toBe("settings");
   });
 
   it('Test10: Clicking the back button, new URL should be /#entry1', async() => {
     // implement test10: Clicking on the back button should update the URL to contain ‘/#entry1’
+    page.goBack();
+    await page.waitForNavigation();
 
+    expect(page.url()).toBe("http://127.0.0.1:5500/#entry1");
+  }, 10000);
+
+  
+  it('Test11: Clicking the back button once should bring the user back to the home page', async() => {
+    // define and implement test11: Clicking the back button once should bring the user back to the home page
+    page.goBack();
+    await page.waitForNavigation();
+
+    expect(page.url()).toBe("http://127.0.0.1:5500/");
+  }, 10000);
+
+  
+  it('Test12: When the user if on the homepage, the header title should be “Journal Entries”', async() => {
+    // define and implement test12: When the user if on the homepage, the header title should be “Journal Entries”
+    const homeTitle = await page.$eval("header > h1", (headers) => headers.innerHTML);
+
+    expect(homeTitle).toBe("Journal Entries");
   });
 
-  // define and implement test11: Clicking the back button once should bring the user back to the home page
 
+  it('Test13: On the home page the <body> element should not have any class attribute', async() => {
+    // define and implement test13: On the home page the <body> element should not have any class attribute 
+    const homeBodyAttr = await page.$eval("body", (body) => body.className);
+    expect(homeBodyAttr).toBe("");
+  });
 
-  // define and implement test12: When the user if on the homepage, the header title should be “Journal Entries”
+  it('Test14: Verify the url is correct when clicking on the second entry', async() => {
+    // define and implement test14: Verify the url is correct when clicking on the second entry
 
+    const entries = await page.$$("journal-entry");
+    // Get the first entry
+    const entry2 = entries[1];
+    
+    await entry2.click();
+    // awaiting navigation
+    await page.waitForNavigation();
+    
+    // Check if page url matches first entry
+    expect(page.url()).toBe("http://127.0.0.1:5500/#entry2");
+  });
 
-  // define and implement test13: On the home page the <body> element should not have any class attribute 
+  
+  it('Test15: Verify the title is current when clicking on the second entry', async () => {
+    // define and implement test15: Verify the title is current when clicking on the second entry
+   
+    const entry2Title = await page.$eval("header > h1", (headers) => headers.innerHTML);
 
+    expect(entry2Title).toBe("Entry 2");
+  });
 
-  // define and implement test14: Verify the url is correct when clicking on the second entry
+  it('Test16: Verify the entry page contents is correct when clicking on the second entry', async () => {
+    /*
+       define and implement test16: Verify the entry page contents is correct when clicking on the second entry
+        { 
+          title: 'Run, Forrest! Run!',
+          date: '4/26/2021',
+          content: "Mama always said life was like a box of chocolates. You never know what you're gonna get.",
+          image: {
+            src: 'https://s.abcnews.com/images/Entertainment/HT_forrest_gump_ml_140219_4x3_992.jpg',
+            alt: 'forrest running'
+          }
+        }
+      */
 
+    const entry = await page.$eval("entry-page", (entry2) => entry2.entry);
 
-  // define and implement test15: Verify the title is current when clicking on the second entry
+    expect(entry).toEqual({ 
+      title: 'Run, Forrest! Run!',
+      date: '4/26/2021',
+      content: "Mama always said life was like a box of chocolates. You never know what you're gonna get.",
+      image: {
+        src: 'https://s.abcnews.com/images/Entertainment/HT_forrest_gump_ml_140219_4x3_992.jpg',
+        alt: 'forrest running'
+      } 
+                });
 
-
-  // define and implement test16: Verify the entry page contents is correct when clicking on the second entry
-
+  }, 10000);
 
   // create your own test 17
+  it('Test17: Clicking the back button once should bring the user back to the home page', async() => {
+    page.goBack();
+    await page.waitForNavigation();
+
+    expect(page.url()).toBe("http://127.0.0.1:5500/");
+  }, 10000);
 
   // create your own test 18
+  it('Test18: Clicking the forward button once should bring the user back to second entry page', async() => {
+    page.goForward();
+    await page.waitForNavigation();
+
+    expect(page.url()).toBe("http://127.0.0.1:5500/#entry2");
+  }, 10000);
 
   // create your own test 19
+  it('Test19: Clicking the header button once in entry view should bring the user back to home page', async() => {
+    page.click('header > h1')
+    await page.waitForNavigation();
+    
+    
+    expect(page.url()).toBe("http://127.0.0.1:5500/");
+  }, 10000);
 
   // create your own test 20
-  
+  it('Test20: Clicking the header button once at home page should not do anything', async() => {
+    page.click('header > h1')
+    await page.waitForNavigation();
+    
+    
+    expect(page.url()).toBe("http://127.0.0.1:5500/");
+  });
+
 });
